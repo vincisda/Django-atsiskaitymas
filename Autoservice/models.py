e77e3d8d586b17c606c82953d5156d4e2a0da2f6
vincisda <80175143+vincisda@users.noreply.github.com>
2022-03-16 20:35:14 +0200

    model Car created

--- Autoservice/models.py
+++ Autoservice/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    state_registration = models.CharField(max_length=20, null=False, )
+    car_model_id       = models.CharField(max_length=100,) 
+    vin_code           = models.CharField(max_length=30,)
+    client             = models.CharField(max_length=100)
+
+
+# class CarModel(models.Model):
+#     make = models.CharField(max_length=100)
+#     model = models.CharField(max_length=100) 
