eec4ae8f61b1d4bf585e1a99c8d160d5451458aa
vincisda <80175143+vincisda@users.noreply.github.com>
2022-03-19 10:41:14 +0200

    templates

--- autoservisas/urls.py
+++ autoservisas/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Autoservice.views import home_view, login_view, register_view
 
 urlpatterns = [
+    path('home/', home_view, name='home_view'),
+    path('login/', login_view, name='login_view'),
+    path('register/', register_view, name='register_view'),
     path('admin/', admin.site.urls),
+    
 ]
